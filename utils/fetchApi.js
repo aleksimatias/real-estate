1330c400ddc2904ddd1033035d6d4c0c9f1ef908
Aleksi Männistö <aatumannisto@gmail.com>
2022-01-28 02:35:20 +0200

    Troubleshooting Vercel

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,8 @@ export const fetchApi = async (url) => {
     const { data } = await axios.get(url, {
         headers: {
             "x-rapidapi-host": "bayut.p.rapidapi.com",
-            "x-rapidapi-key": `${process.env.API_KEY}`,
+            "x-rapidapi-key":
+                "7f81defeffmsh98cb175c51cb477p1d2d8bjsn5978cf921176",
         },
     });
 
